44a2831d7952f93e90104b77e552eaa4af3474fc
Emmet Murray <emmet.murray@etrade.com>
2019-06-07 16:45:46 -0500

    Initial Commit

---
+++ PokerRecognizerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokerRecognizerApp
+//
+//  Created by Murray, Emmet on 6/7/19.
+//  Copyright © 2019 Murray, Emmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
